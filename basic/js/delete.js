23287cb99b39079013641cbbf898f1e472a44df7
MyznEiji <em711th@icloud.com>
2018-04-09 18:01:36 +0900

    fine : basic

---
+++ basic/js/delete.js
@@ -0,0 +1,9 @@
+var x = 10;
+print(delete x);
+print(x);
+
+var r = ["hoge", "fuga"];
+print(delete r[0]);
+
+var d = {x:1, x:2};
+print(delete r.x);
---
+++ basic/js/typeof.js
@@ -0,0 +1,9 @@
+var num = 1;
+print(typeof num);
+
+var str = "hello";
+print(typeof str);
+
+
+var flag = true;
+print(typeof flag);
